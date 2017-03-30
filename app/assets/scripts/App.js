0a89e4ab3404e9cd7dacaf5137cf0c3eb1571a80
DESKTOP-NUN5RK4\김주형 <14wnrkim@gmail.com>
2017-03-30 18:01:31 +0900

    Completed the modal overlay.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,11 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 import $ from 'jquery';
 
 new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
 let stickyHeader = new StickyHeader();
+let modal = new Modal();
